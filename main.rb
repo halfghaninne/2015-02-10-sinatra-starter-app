d3246a1f06e72b7f476675562d1ed9be49ed0eaf
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-02-10 21:42:26 -0600

    Added some extras from assignment, commented out extras features that need de-bugging.

--- main.rb
+++ main.rb
@@ -1,16 +1,34 @@
 require "sinatra"
 
+# Tried to move the Ruby here, and the program didn't recognize the 'local variable' params
+#
+# module Calculation
+#   @result = (@amount / @gallons).round(2)
+# end
+#
+# helpers Calculation
+
 get "/" do
-  erb :homepage, :layout => :template
+  erb :homepage
 end
 
 get "/calc" do
-  erb :calculator, :layout => :template
+  erb :calculator
 end
+
+# before "/result" do
+#   if params[:amount] == nil || params[:gallons] == nil
+#     redirect to "/calc"
+#   end
+# end
 
 get "/result" do
   @result = (params[:amount].to_f / params[:gallons].to_f).round(2)
-  erb :result, :layout => :template
+  
+  # @amount = params[:amount].to_f
+  # @gallons = params[:gallons].to_f
+  
+  erb :result
 end
 
 
