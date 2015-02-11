60fab51ede1bbc4f3bf5fe6a6b9fc38a1bf3b776
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-02-10 21:51:13 -0600

    Working Helper module.

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
 require "sinatra"
 
 # Tried to move the Ruby here, and the program didn't recognize the 'local variable' params
-#
-# module Calculation
-#   @result = (@amount / @gallons).round(2)
-# end
-#
-# helpers Calculation
+
+module Calculation
+  def calculate(amount, gallons)
+    (amount.to_f / gallons.to_f).round(2)
+  end
+end
+
+helpers Calculation
 
 get "/" do
   erb :homepage
@@ -16,14 +18,15 @@ get "/calc" do
   erb :calculator
 end
 
+###NOT WORKING: Does not redirect; leads to /result and shows @result as NaN###
 # before "/result" do
-#   if params[:amount] == nil || params[:gallons] == nil
+#   if params[:amount] == nil # || params[:gallons] == nil
 #     redirect to "/calc"
 #   end
 # end
 
 get "/result" do
-  @result = (params[:amount].to_f / params[:gallons].to_f).round(2)
+  @result = calculate(params[:amount], params[:gallons])
   
   # @amount = params[:amount].to_f
   # @gallons = params[:gallons].to_f
